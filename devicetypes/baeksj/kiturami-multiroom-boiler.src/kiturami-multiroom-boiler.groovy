428dcc60bd55bf78c242bbe1ee398c6d0152d043
SeungJin Baek <baeksj@gmail.com>
2021-10-26 23:53:57 +0900

    add function for schedule mode

--- devicetypes/baeksj/kiturami-multiroom-boiler.src/kiturami-multiroom-boiler.groovy
+++ devicetypes/baeksj/kiturami-multiroom-boiler.src/kiturami-multiroom-boiler.groovy
@@ -262,7 +262,7 @@ def executeKrbAway(slaveId) {
 }
 
 def executeKrbSchedule(slaveId) {
-    def controlMessage = [messageId: "0108", messageBody: "${slaveId}000000001E"]
+    def controlMessage = [messageId: "0108", messageBody: "${slaveId}000000021E"]
     log.debug "Schedule: ${controlMessage}"
     executeAPICommand(getOperation(controlMessage).deviceControl, controlCallback)
 }
